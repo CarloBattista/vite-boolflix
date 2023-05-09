ff8773b0705fe63286d0bf8d0f460b844c22c991
CarloBattista <carlitobatti@gmail.com>
2023-05-09 11:33:16 +0200

    reset + creazione componenti

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Header</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeaderComp",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Main</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MainComp",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped></style>
